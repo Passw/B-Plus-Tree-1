fe2fe17918e61a229686c9f985d2ef950a506885
Passw <Passw_Passw@163.com>
2021-03-03 10:14:04 +0800

    string,string

--- src/example.cpp
+++ src/example.cpp
@@ -5,25 +5,29 @@ using namespace BPlusTree;
 using namespace std;
 
 int main() {
-  Tree<double, string> t;
-  Tree<double, string>::iterator it;
-  Tree<double, string>::reverse_iterator rit;
+  // Tree<double, string> t;
+  // Tree<double, string>::iterator it;
+  // Tree<double, string>::reverse_iterator rit;
 
-  t.insert(4, "A"); // insert (4,A) into B+Tree
-  t.insert(2, "B"); // insert (2,B) into B+Tree
+  Tree<string, string> t;
+  Tree<string, string>::iterator it;
+  Tree<string, string>::reverse_iterator rit;
 
-  t[1] = "C"; // insert (1,C) into B+Tree
-  t[0] = "D"; // insert (0,D) into B+Tree
+  t.insert("a", "A"); // insert (a,A) into B+Tree
+  t.insert("b", "B"); // insert (b,B) into B+Tree
 
-  cout << t[2] << endl;
+  t["c"] = "C"; // insert (c,C) into B+Tree
+  t["d"] = "D"; // insert (d,D) into B+Tree
+
+  cout << t["b"] << endl;
   try {
-    cout << t.at(3)
-         << endl; // 3 is not in B+Tree. It will throw out_of_range exception.
+    cout << t.at("e")
+         << endl; // e is not in B+Tree. It will throw out_of_range exception.
   } catch (std::out_of_range e) {
     cout << e.what() << endl;
   }
 
-  cout << t.contains(2) << " " << t.contains(5) << endl;
+  cout << t.contains("a") << " " << t.contains("c") << endl;
 
   // ascending order
   for (it = t.begin(); it != t.end(); ++it) {
